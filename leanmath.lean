7f07b1daf55539e525401b62244d4720c8937b88
Isaac Cheng <48893075+casithepython@users.noreply.github.com>
2024-03-31 20:17:18 -0500

    removed two files bc errors

--- leanmath.lean
+++ leanmath.lean
@@ -4,5 +4,3 @@ import «leanmath».Common
 import «leanmath».Ch02.S01_Calculating
 import «leanmath».Ch02.S02_Proving_Identities_in_Algebraic_Structures
 import «leanmath».Ch02.S03_Using_Theorems_and_Lemmas
-import «leanmath».Ch02.S04_More_on_Order_and_Divisibility
-import «leanmath».Ch02.S05_Proving_Facts_about_Algebraic_Structures
